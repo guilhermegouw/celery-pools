d9ecf1911dc355bfca006d6e74753fa8f145c40a
guilhermegouwloft <guilherme.gouw@loft-ext.com>
2024-05-22 06:25:18 -0300

    22-05-2024

--- django_celery_example/celery.py
+++ django_celery_example/celery.py
@@ -3,7 +3,8 @@ from celery import Celery
 from django.conf import settings
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_example.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'django_celery_example.settings')
 app = Celery('django_celery_example')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
@@ -14,4 +15,3 @@ def divide(x, y):
     import time
     time.sleep(5)
     return x / y
-
